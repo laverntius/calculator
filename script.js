9edd50895056d65d118907368f3e2417e185003c
laverntius <narugo901@gmail.com>
2017-02-23 16:24:16 +0000

    yay

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
-        alert("javascript runs this code when the page loads");
-        
-        function clearDisplay(){
-        document.getElementById("display-bar").innerHTML = "";
-        };
-        
-        function display(userInput){
-            document.getElementById("display-bar").innerHTML = userInput
-        }
+
+       function clearDisplay(){
+    document.getElementById("display-bar").value = "";
+}
+
+function display(userInput){
+    document.getElementById("display-bar").value += userInput;
+}
+
+function calculate(){
+    var answer = eval(document.getElementById("display-bar").value);
+    document.getElementById("display-bar").value = answer;
+}
+
+function jamirSucks(){
+    alert("You have insulted the great creator and now must suffer the alphabet");
+alert("a");
+    alert("A");
+    alert("B");
+    alert("C");
+    alert("D");
+    alert("E");
+    alert("F");
+    alert("G");
+    alert("H");
+    alert("I");
+    alert("J");
+    alert("K");
+    alert("L");
+    alert("M");
+    alert("N");
+    alert("O");
+    alert("P");
+    alert("Q");
+    alert("R");
+    alert("S");
+    alert("T");
+    alert("U");
+    alert("V");
+    alert("W");
+    alert("X");
+    alert("Y");
+    alert("and Z");
+    alert("Now you know your");
+    alert("ABC's");
+    alert("Next time just be ");
+    alert("Kind to me");
+    alert("try saying something like jamirIsTheBest");
+    
+}
+function jamirIsTheBest(){
+    alert("I appreciate the compliment and you have appeased me. That is all.")
+}
